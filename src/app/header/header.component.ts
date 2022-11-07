05f9d64e3a30a7b1a7df9d7192167335e8455473
syedsyez <110734307+syedsyez@users.noreply.github.com>
2022-11-07 06:41:48 +0530

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component,OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit,OnDestroy {
+
+  constructor() { }
+  
+
+  ngOnInit(): void {
+ 
+  }
+
+  onHeaderSelect(){
+
+}
+onSaveData(){
+  
+}
+onFetchData(){
+  
+}
+onLogOut(){
+
+}
+
+ngOnDestroy(): void {
+
+}
+
+}
